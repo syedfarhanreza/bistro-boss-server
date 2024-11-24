28cd7dfb004a9c2a1254ee9b41ccb29c04838deb
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-11-24 23:18:17 +0600

    get api for cart created

--- index.js
+++ index.js
@@ -39,6 +39,12 @@ async function run() {
         res.send(result);
     });
     // cart collection api
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+       const result = await cartCollection.find(query).toArray();
+       res.send(result);
+    })
     app.post('/carts', async(req, res)=> {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
